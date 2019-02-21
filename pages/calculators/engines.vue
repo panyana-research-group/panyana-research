148387a6718cdcea310d221a4a9360b903c50601
Jake Potrebic <jake.m.potrebic@gmail.com>
2019-02-21 15:35:48 -0700

    updater oh calcs

--- pages/calculators/engines.vue
+++ pages/calculators/engines.vue
@@ -200,22 +200,22 @@ export default {
     ohTime(pwr, oh, cf) {
       return Math.round(
         Math.log(
-          (0.755234428801821 *
-            Math.pow(parseInt(pwr), 1.42) *
-            Math.pow(parseInt(oh), -1.08)) /
-            (0.755234428801821 *
-              Math.pow(parseInt(pwr), 1.42) *
-              Math.pow(parseInt(oh), -1.08) -
+          (0.712430929124863 *
+            Math.pow(parseInt(pwr), 1.479) *
+            Math.pow(parseInt(oh), -1.131)) /
+            (0.712430929124863 *
+              Math.pow(parseInt(pwr), 1.479) *
+              Math.pow(parseInt(oh), -1.131) -
               500 *
                 (0.0016915 *
                   Math.pow(parseInt(cf), 0.5) *
-                  Math.pow(parseInt(oh), 0.25) *
-                  Math.pow(parseInt(pwr), -0.335)))
+                  Math.pow(parseInt(oh), 0.245) *
+                  Math.pow(parseInt(pwr), -0.33)))
         ) /
           (0.0016915 *
             Math.pow(parseInt(cf), 0.5) *
-            Math.pow(parseInt(oh), 0.25) *
-            Math.pow(parseInt(pwr), -0.335))
+            Math.pow(parseInt(oh), 0.245) *
+            Math.pow(parseInt(pwr), -0.33))
       )
     },
     round(value, decimals) {
