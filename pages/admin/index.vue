3b2e85a9acf0043b6d801403a6cab3e5b5191b48
Jake Potrebic <jake.m.potrebic@gmail.com>
2019-04-30 22:40:55 -0700

    admin index

---
+++ pages/admin/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs12 lg8>
+      <v-card>
+        <v-card-text class="headline text-xs-center">
+          Users
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  name: 'AdminHome',
+  middleware: 'admin',
+  mounted() {
+    this.$api
+      .get('/auth/users/all')
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(console.error)
+  }
+}
+</script>
