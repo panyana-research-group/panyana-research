9a2888480da0b128cdac4875603e559307183558
Jake Potrebic <jake.m.potrebic@gmail.com>
2019-05-09 12:10:09 -0700

    added head for overheat calc

--- pages/calculators/overheat.vue
+++ pages/calculators/overheat.vue
@@ -11,6 +11,19 @@ export default {
   name: 'OverheatCalc',
   components: {
     'engine-oh-time-calc': EngineOverheatTimeCalc
+  },
+  head() {
+    return {
+      title: 'Engine Overheat Time',
+      meta: [
+        { hid: 'og:title', name: 'og:title', content: 'Engine Overheat Time' },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: 'Calculates the overheat time for engines'
+        }
+      ]
+    }
   }
 }
 </script>
