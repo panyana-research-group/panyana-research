12d483664b717cd1c8a2933d892c2c47e4b95091
Jake Potrebic <jake.m.potrebic@gmail.com>
2019-05-23 20:20:59 -0700

    fix sorting issue with cannon page

--- pages/materials/cannonswivels.vue
+++ pages/materials/cannonswivels.vue
@@ -17,21 +17,21 @@ export default {
       ],
       headers: {
         firing: [
-          { text: 'Power', value: 'boosts["cannon/swivel"].firing.pwr' },
-          { text: 'Rate Of Fire', value: 'boosts["cannon/swivel"].firing.rof' }
+          { text: 'Power', value: 'boosts.cannon/swivel.firing.pwr' },
+          { text: 'Rate Of Fire', value: 'boosts.cannon/swivel.firing.rof' }
         ],
         barrel: [
-          { text: 'Power', value: 'boosts["cannon/swivel"].firing.pwr' },
-          { text: 'Capacity', value: 'boosts["cannon/swivel"].firing.cap' },
-          { text: 'Overheat Limit', value: 'boosts["cannon/swivel"].firing.oh' }
+          { text: 'Power', value: 'boosts.cannon/swivel.barrel.pwr' },
+          { text: 'Capacity', value: 'boosts.cannon/swivel.barrel.cap' },
+          { text: 'Overheat Limit', value: 'boosts.cannon/swivel.barrel.oh' }
         ],
         ammo: [
-          { text: 'Capacity', value: 'boosts["cannon/swivel"].firing.cap' },
+          { text: 'Capacity', value: 'boosts.cannon/swivel.ammo.cap' },
           {
             text: 'Overheat Limit',
-            value: 'boosts["cannon/swivel"].firing.oh'
+            value: 'boosts.cannon/swivel.ammo.oh'
           },
-          { text: 'Rate Of Fire', value: 'boosts["cannon/swivel"].firing.rof' }
+          { text: 'Rate Of Fire', value: 'boosts.cannon/swivel.ammo.rof' }
         ]
       }
     }
