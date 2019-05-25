ec05b819f988a0e280d37a75b15af1e497cd83fe
PanyanaResearch <panyanaresearch@gmail.com>
2019-05-25 00:01:35 +0000

    Automated deployment to GitHub Pages: 654762e4cd0737a137851f57c9cdb9b75f03f8f8

---
+++ _nuxt/77e47e39dd91b1235cb0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{523:function(e,t,n){"use strict";n.r(t);var l={name:"TestEmbed",layout:"embed"},c=n(40),component=Object(c.a)(l,function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("Hey")])},[],!1,null,null,null);t.default=component.exports}}]);
---
+++ js/google-analytics.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || []
+function gtag() {
+  window.dataLayer.push(arguments)
+}
+gtag('js', new Date())
+
+gtag('config', 'UA-135117645-1')
